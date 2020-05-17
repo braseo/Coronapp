784b03040d01f8fde43932e6a8b8093dba5f8c7c
braseo <62137876+braseo@users.noreply.github.com>
2020-05-17 18:50:05 +0200

    Ajout Singletons pour les objects

---
+++ app/src/main/java/com/example/coronapp/Constants.java
@@ -0,0 +1,8 @@
+package com.example.coronapp;
+
+public class Constants {
+
+        public static final String BASE_URL = "https://api.covid19api.com/";
+
+        public static final String KEY_CORONA_LIST = "jsonCoronaList";
+}
