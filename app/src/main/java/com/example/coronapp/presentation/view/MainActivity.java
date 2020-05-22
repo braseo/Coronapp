4fd7be9478dac9b7c5f402e84479fe89d1aa0e49
braseo <62137876+braseo@users.noreply.github.com>
2020-05-22 11:22:54 +0200

    Ajout d'une barre de recherche pour filtrer la recycle viex et ajout d'une barre de menu avec des items Settings et exit pour quitter

--- app/src/main/java/com/example/coronapp/presentation/view/MainActivity.java
+++ app/src/main/java/com/example/coronapp/presentation/view/MainActivity.java
@@ -6,6 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.example.coronapp.R;
@@ -13,6 +20,7 @@ import com.example.coronapp.Singletons;
 import com.example.coronapp.presentation.controller.MainController;
 import com.example.coronapp.presentation.model.Corona;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerViewAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    private List<Corona> values;
+
 
     private MainController controller;
 
@@ -35,6 +46,54 @@ public class MainActivity extends AppCompatActivity {
         );
         controller.onStart();
 
+        EditText editText = findViewById(R.id.edittext);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void filter(String text) {
+        List<Corona>filteredList = new ArrayList();
+        for (Corona item : values) {
+            if (item.getCountry().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+        mAdapter.filterList(filteredList);
     }
 
     public void showList(List<Corona> coronaList) {
@@ -64,4 +123,7 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.this.startActivity(myIntent);
 
     }
+
+
+
 }
