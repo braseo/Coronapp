05fe5a0cf69a76056f13ad26b7578a09f493493c
braseo <62137876+braseo@users.noreply.github.com>
2020-05-23 18:59:49 +0200

    Ajout des notifications Push avec Firebase  et modification graphique, design

--- app/src/main/java/com/example/coronapp/presentation/view/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/coronapp/presentation/view/RecyclerViewAdapter.java
@@ -1,14 +1,18 @@
 package com.example.coronapp.presentation.view;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.coronapp.R;
 import com.example.coronapp.presentation.model.Corona;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -25,12 +29,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         TextView txtHeader;
         TextView txtFooter;
         View layout;
+        ImageView icon;
 
         ViewHolder(View v) {
             super(v);
             layout = v;
             txtHeader = (TextView) v.findViewById(R.id.firstLine);
             txtFooter = (TextView) v.findViewById(R.id.secondLine);
+            icon = (ImageView) v.findViewById(R.id.icon);
+
         }
     }
 
@@ -53,8 +60,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                             int viewType) {
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View v = inflater.inflate(R.layout.row_layout, parent, false);
         ViewHolder vh = new ViewHolder(v);
@@ -66,6 +72,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         final Corona currentCorona = values.get(position);
         holder.txtHeader.setText(currentCorona.getCountry());
         holder.txtFooter.setText("Click for more info");
+
+        Picasso.get()
+                .load("https://image.flaticon.com/icons/png/512/46/46649.png")
+                .resize(200, 200)
+                .into(holder.icon);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
